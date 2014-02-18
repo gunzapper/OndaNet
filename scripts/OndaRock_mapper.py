c373c62067c96497b3bc60108b460fe1ce669c9e
Pietro Brunetti <gunzapper@gmail.com>
2014-02-18 15:57:13 +0100

    Nice work about re pattern

--- scripts/OndaRock_mapper.py
+++ scripts/OndaRock_mapper.py
@@ -5,22 +5,42 @@ using a pratical class
 
 __author__ = 'Pietro Brunetti aka gunzapper'
 
+import re
+# a list of patterns
+album_pattern = re.compile(r'(recensioni)/(\d{4})_(\w+)\.htm$')
+pietre_miliari_pattern = re.compile(r'(pietremiliari)/(\w+)\.htm$')
+artist_pattern = re.compile(r'(rockedintorni|popmuzik|dark|songwriter|altrisuoni|italia|jazz)/(\w+)\.htm$')
+gender_pattern = re.compile(r'(storiadelrock)/(\w+)\.htm$')
+
 import requests
 from bs4 import BeautifulSoup
 
+class OndaRock_mapper_error(Exception):
+    pass
+
+class OndaRockAlbum_Error(OndaRock_mapper_error):
+    pass
+
 class OndaRockAlbum_parser:
     def __init__(self, url):
+        s = album_pattern.search(url)
+        if not s:
+            raise OndaRockAlbum_Error, "The url {0} does not match the right pattern".format(url)
         self.url = url
-        soup = self.__get_soup()
-        self.artist = soup.body.h1.text
-        self.title = soup.body.h2.text
-        dati = soup.body.div(id="dati")[0].text
-        # for example u'2010 (Bela Union) | folk-rock'
-        self.year = dati[0:4]
-        op_br = dati.find('(')+1
-        cl_br = dati.find(')')
-        self.label = dati[op_br:cl_br]
-        self.genders = dati[cl_br+3:].split(',')
+        try:
+            soup = self.__get_soup()
+        except AssertionError:
+            print("Page {0} no found".format(self.url))
+        else:
+            self.artist = soup.body.h1.text
+            self.title = soup.body.h2.text
+            dati = soup.body.div(id="dati")[0].text
+            # for example u'2010 (Bela Union) | folk-rock'
+            self.year = dati[0:4]
+            op_br = dati.find('(')+1
+            cl_br = dati.find(')')
+            self.label = dati[op_br:cl_br]
+            self.genders = dati[cl_br+3:].split(',')
 
     def __repr__(self):
         return "<OndaRockPage_parser of {0}>".format(url)
@@ -29,8 +49,7 @@ class OndaRockAlbum_parser:
         """
         for BeautifulSoup works
 
-        :param url: the url of a web page
-        :type url: str
+        :param self:
         :return: BeautifulSoup parser
         :rtype: BeautifulSoup
         """
@@ -45,17 +64,14 @@ class OndaRockAlbum_parser:
         Starting from a web page
         iter_links iterates for each anchor in the page
 
-        :param soup: the parser for the page
-        :type url: BeautifulSoup
-        :param root: the page 'home'
-        :type root: str
+        :param self:
         """
         for anchor in self.__get_soup().findAll('a'):
             link = anchor.get('href')
             yield link
 
 if __name__ == "__main__":
-    example = OndaRockPage_parser("http://www.ondarock.it/recensioni/2010_midlake.htm")
+    example = OndaRockAlbum_parser("http://www.ondarock.it/recensioni/2010_midlake.htm")
     print(example.artist)
     print(example.title)
     print(example.genders)
